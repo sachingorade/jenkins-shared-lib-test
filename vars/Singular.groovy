33caab01f3e57a322b403c9e5845f105c1987a24
Sachin Gorade <sachin.gorade@gmail.com>
2019-12-12 17:01:15 +0100

    Updated Singular.groovy
    Updated file to check if variable name works in stage name.

--- vars/Singular.groovy
+++ vars/Singular.groovy
@@ -2,20 +2,23 @@ def call(serviceName) {
 
     pipeline {
         agent any
+        environment {
+            SERVICE_NAME = env.JOB_BASE_NAME;
+        }
         stages {
-            stage(env.JOB_BASE_NAME + ' Checkout github') {
+            stage('$SERVICE_NAME Checkout github') {
                 steps {
                     checkout scm
                 }
             }
 
-            stage(env.JOB_BASE_NAME + ' Build') {
+            stage('$SERVICE_NAME Build') {
                 steps {
                     sh './mvnw clean package -DskipTests=true'
                 }
             }
 
-            stage(env.JOB_BASE_NAME + ' Test') {
+            stage('$SERVICE_NAME Test') {
                 steps {
                     sh './mvnw clean verify'
                 }
